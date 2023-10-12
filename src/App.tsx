c3219b8ccf957c2bd1b393702e5d137f6061a8b3
Nicolas Gravertt <nicolasgravertt@hotmail.com>
2023-10-11 21:53:03 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
-// import YoutubeDisplayer from "./pages/YoutubeDisplayer/YoutubeDisplayer";
+import YoutubeDisplayer from "./pages/YoutubeDisplayer/YoutubeDisplayer";
 // import Login from "./pages/auth/Login/Login";
 // import Register from "./pages/auth/Register/Register";
-import PasswordRecovery from "./pages/auth/PasswordRecovery/PasswordRecovery";
+// import PasswordRecovery from "./pages/auth/PasswordRecovery/PasswordRecovery";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<YoutubeDisplayer />}></Route> */}
+      <Route path="/" element={<YoutubeDisplayer />}></Route>
       {/* <Route path="/" element={<Login />}></Route> */}
       {/* <Route path="/" element={<Register />}></Route> */}
-      <Route path="/" element={<PasswordRecovery />}></Route>
+      {/* <Route path="/" element={<PasswordRecovery />}></Route> */}
     </Routes>
   );
 }
